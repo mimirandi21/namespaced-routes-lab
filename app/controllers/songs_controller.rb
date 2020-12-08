81ae3a2c00fd888feb726494c9ca26bd511a9daf
Randi Schreiner <mimirandi21@gmail.com>
2020-12-08 17:01:22 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,6 @@
 class SongsController < ApplicationController
   def index
+    @preference = Preference.first
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
@@ -9,6 +10,14 @@ class SongsController < ApplicationController
       end
     else
       @songs = Song.all
+    end
+
+    if @preference
+      if @preference.song_sort_order = "ASC"
+        @songs.sort_by!{|song| song.title}
+      else
+        @songs.sort_by!{|song| song.title}.reverse
+      end
     end
   end
 
@@ -25,7 +34,12 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    @preference = Preference.first
+    if @preference.allow_create_songs
+      @song = Song.new
+    else
+      redirect_to songs_path, alert: "Song creation not allowed"
+    end
   end
 
   def create
